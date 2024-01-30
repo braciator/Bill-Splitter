587b2b8804b48354fbf0bf15a773aabdacfa89ea
Daniel <kuchta.daniel1@gmail.com>
2024-01-30 23:43:55 +0100

    Created constant values

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+const price = document.querySelector('#price')
+const people = document.querySelector('#people')
+const tip = document.querySelector('#tip')
+const btn = document.querySelector('.count')
+const error = document.querySelector('.error')
+const reply = document.querySelector('.cost-info')
+const cost = document.querySelector('.cost')
 
